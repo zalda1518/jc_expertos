aa6cd8fa6a50423ba3cdcfb2010fa9900b1a357c
zalda1518 <respaldos1992@gmail.com>
2025-10-22 09:00:20 -0500

    casi 2

--- vistas/encontrado.php
+++ vistas/encontrado.php
@@ -1,6 +1,5 @@
 <?php require('header.php');
-require('../controlador/MainController.php'); 
-require_once('../controlador/Redirect.php');
+require('../controlador/MainController.php');
 require_once('../includes/alertas.php'); ?>
 <script src="../includes/app.js"></script>
 
@@ -8,15 +7,15 @@ require_once('../includes/alertas.php'); ?>
 <div id="main-encontrado">
   <div class="padre-menu-desktop">
     <?php require('menuDesktop.php') ?>
-    </div>
-
-   <div class="padre-hamburguesa"> 
-   <img src="../public/menu.png"  class="hamburguesa" id="hamburguesa" onclick="Menu()">
   </div>
 
-  <?php require('menuResponsive.php')?> 
+  <div class="padre-hamburguesa">
+    <img src="../public/menu.png" class="hamburguesa" id="hamburguesa" onclick="Menu()">
+  </div>
 
-   <table border="solid 1px" id="table-encontrado">
+  <?php require('menuResponsive.php') ?>
+
+  <table border="solid 1px" id="table-encontrado">
     <thead>
       <th class="columnas">ID</th>
       <th class="columnas">ATENDIO</th>
@@ -31,50 +30,53 @@ require_once('../includes/alertas.php'); ?>
       <th class="columnas">BORRAR</th>
     </thead>
 
-    <?php 
+    <?php
 
-      if(empty($_POST['buscar-cedula'])){
-          echo "<script>alert('Escriba el numero de cedula')</script>";
-          new Redirect('buscar.php');
-       }
-        
-        if(isset($_POST['buscar'])){
-          $dataa = $_POST; 
-          $obj = new MainController();
-          $dat = $obj->search($dataa);
-          $datos = $dat;
+    if (empty($_POST['buscar-cedula'])) {
+      echo "<script>alert('Escriba el numero de cedula')</script>";
+      require_once('../controlador/Redirect.php');
+      new Redirect('buscar.php');
+    }
+
+    if (isset($_POST['buscar'])) {
+      $dataa = $_POST;
+      $obj = new MainController();
+      $dat = $obj->search($dataa);
+      $datos = $dat;
 
 
-        if(empty($dat)){
-         $var = $_POST['buscar-cedula'];
+      if (empty($dat)) {
+        $var = $_POST['buscar-cedula'];
         echo "<script>alert('Cedula no encontrada')</script>";
-         new Redirect('buscar.php');
-          die;
-       } ?>
+        require_once('../controlador/Redirect.php');
+        new Redirect('buscar.php');
+        die;
+      } ?>
 
-        <?php foreach ($datos as $valor) {  ?>
-     <tbody>
-        <td><?php echo $valor['id'] ?></td>
-        <td><?php echo $_SESSION['start'] ?></td>
-        <td><?php echo $valor['fecha_creacion'] ?></td>
-        <td><?php echo $valor['nombres'] ?></td>
-        <td><?php echo $valor['cedula'] ?></td>
-        <td><?php echo $valor['direccion'] ?></td>
-        <td><?php echo $valor['telefono'] ?></td>
-        <td><?php echo $valor['pedido'] ?></td>
-        <td><?php echo $valor['servicio'] ?></td>
-        <td class='columnas' id="editar-tabla"><a href="VistaEditar.php?itemE=<?php echo $valor['id'] ?>"><img src="../public/editar.png" width="25px"></a>
-        </td>
-        <td class='columnas' id="eliminar-tabla"><a href="../controlador/MainController.php?delete=<?php echo $valor['id'] ?>"><img src="../public/eliminar.png" width="25px"></a>
-        </td>
+      <?php foreach ($datos as $valor) {  ?>
+        <tbody>
+          <td><?php echo $valor['id'] ?></td>
+          <td><?php echo $_SESSION['start'] ?></td>
+          <td><?php echo $valor['fecha_creacion'] ?></td>
+          <td><?php echo $valor['nombres'] ?></td>
+          <td><?php echo $valor['cedula'] ?></td>
+          <td><?php echo $valor['direccion'] ?></td>
+          <td><?php echo $valor['telefono'] ?></td>
+          <td><?php echo $valor['pedido'] ?></td>
+          <td><?php echo $valor['servicio'] ?></td>
+          <td class='columnas' id="editar-tabla"><a href="VistaEditar.php?itemE=<?php echo $valor['id'] ?>"><img src="../public/editar.png" width="25px"></a>
+          </td>
+          <td class='columnas' id="eliminar-tabla"><a href="../controlador/MainController.php?delete=<?php echo $valor['id'] ?>"><img src="../public/eliminar.png" width="25px"></a>
+          </td>
 
-      </tbody>  
-    
-     <?php  } }?> 
-    </table>
-    
+        </tbody>
 
-</div> 
+    <?php  }
+    } ?>
+  </table>
+
+
+</div>
 
 </body>
 
