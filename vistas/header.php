5bd418b105f22ea9824ba5094421bbb01a4d89f2
zalda1518 <respaldos1992@gmail.com>
2025-10-22 18:16:23 -0500

    ya casi 2

--- vistas/header.php
+++ vistas/header.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>mis seguimientos</title>
+    <title>INVENTARIOS</title>
     <link rel="stylesheet" href="../includes/index.css" />
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   </head>
